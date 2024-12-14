6bcf2642fa405dcd479a63dab87600c612f7e4b2
andy3400 <grayhat3400@gmail.com>
2024-12-15 04:14:24 +0900

    Init : Initial Commit

---
+++ portfolio/src/test/java/org/andy/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.andy.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
